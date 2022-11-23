c3ec46ee6771006c4b3317e7ee3cd07fb5fd1549
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-11-23 10:58:02 +0100

    ObservableObject & ObservableProperty

--- ImportApp.WPF/State/Navigators/Navigator.cs
+++ ImportApp.WPF/State/Navigators/Navigator.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using ImportApp.WPF.Commands;
 using ImportApp.WPF.ViewModels;
 using System;
@@ -11,31 +12,14 @@ using System.Windows.Input;
 
 namespace ImportApp.WPF.State.Navigators
 {
-    public partial class Navigator : INavigator, INotifyPropertyChanged
+    public partial class Navigator : ObservableObject, INavigator
     {
-
+        [ObservableProperty]
         private BaseViewModel _currentViewModel;
 
-        public BaseViewModel CurrentViewModel
-        {
-            get
-            {
-                return _currentViewModel;
-            }
-            set
-            {
-                _currentViewModel = value;
-                OnPropertyChanged(nameof(CurrentViewModel));
-            }
-        }
-
+        
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+       
     }
 }
