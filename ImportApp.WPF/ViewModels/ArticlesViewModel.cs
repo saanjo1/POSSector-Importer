17258f6ae3b5e38b6bb2b5d3fd7b93b2c4b54bb8
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-11-29 14:58:56 +0100

    Filter for Articles

--- ImportApp.WPF/ViewModels/ArticlesViewModel.cs
+++ ImportApp.WPF/ViewModels/ArticlesViewModel.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace ImportApp.WPF.ViewModels
 {
@@ -22,6 +23,22 @@ namespace ImportApp.WPF.ViewModels
         [ObservableProperty]
         private ICollection<Article> articleList;
 
+        private string textToFilter;
+
+        public string TextToFilter
+        {
+            get { return textToFilter; }
+            set
+            {
+                textToFilter = value;
+                OnPropertyChanged(nameof(TextToFilter));
+                ArticleCollection.Filter = FilterFunction;
+            }
+        }
+
+
+        [ObservableProperty]
+        private ICollectionView articleCollection;
 
         private IArticleService _articleService;
 
@@ -29,6 +46,17 @@ namespace ImportApp.WPF.ViewModels
         public void LoadData()
         {
             ArticleList = _articleService.GetAll().Result;
+            ArticleCollection = CollectionViewSource.GetDefaultView(ArticleList);
+        }
+
+        private bool FilterFunction(object obj)
+        {
+            if (!string.IsNullOrEmpty(TextToFilter))
+            {
+                var filt = obj as Article;
+                return filt != null && filt.Name.Contains(TextToFilter);
+            }
+            return true;
         }
 
         public ArticlesViewModel(IArticleService articleService)
