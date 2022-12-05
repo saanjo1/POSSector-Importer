a22d6d649f0dd7d0683448a2d881af25f48ea318
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-12-05 13:08:06 +0100

    Pagination / Dugme za pregled stranice prije ne prelazi u enabled iz disabled.

--- ImportApp.WPF/ViewModels/ArticlesViewModel.cs
+++ ImportApp.WPF/ViewModels/ArticlesViewModel.cs
@@ -2,8 +2,11 @@
 using CommunityToolkit.Mvvm.Input;
 using ImportApp.Domain.Models;
 using ImportApp.EntityFramework.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Data;
 
 namespace ImportApp.WPF.ViewModels
@@ -12,8 +15,14 @@ namespace ImportApp.WPF.ViewModels
     [ObservableObject]
     public partial class ArticlesViewModel : BaseViewModel
     {
-        [ObservableProperty]
-        private ICollection<Article> articleList;
+        private IArticleService _articleService;
+        public string Count { get { return articleList.Count + " articles found"; } }
+
+        public ArticlesViewModel(IArticleService articleService)
+        {
+            _articleService = articleService;
+            LoadData();
+        }
 
         private string textToFilter;
 
@@ -30,15 +39,127 @@ namespace ImportApp.WPF.ViewModels
 
 
         [ObservableProperty]
+        private ICollection<Article> articleList;
+
+        [ObservableProperty]
+        private ObservableCollection<Article> articlesCollection = new ObservableCollection<Article>();
+
+        [ObservableProperty]
         private ICollectionView articleCollection;
 
-        private IArticleService _articleService;
+        private int currentPage = 1;
+        public int CurrentPage
+        {
+            get { return currentPage; }
+            set
+            {
+                currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+                UpdateEnableState();
+            }
+        }
+
+        private int selectedRecord = 10;
+        public int SelectedRecord
+        {
+            get { return selectedRecord; }
+            set
+            {
+                selectedRecord = value;
+                OnPropertyChanged(nameof(SelectedRecord));
+                UpdateRecordCount();
+            }
+        }
+
+        private void UpdateRecordCount()
+        {
+            NumberOfPages = (int)Math.Ceiling((double)articleList.Count / SelectedRecord);
+            NumberOfPages = NumberOfPages == 0 ? 1 : NumberOfPages;
+            UpdateCollection(articleList.Take(SelectedRecord));
+            CurrentPage = 1;
+        }
+
+        [ObservableProperty]
+        private int numberOfPages = 10;
+
+        [ObservableProperty]
+        private bool isFirstEnabled;
+        
+        [ObservableProperty]
+        private bool isPreviousEnabled;
+
+        [ObservableProperty]
+        private bool isNextEnabled;
+
+        [ObservableProperty]
+        private bool isLastEnabled;
+
+
+        public static int RecordStartForm = 0;
+        [RelayCommand]
+        private void PreviousPage(object obj)
+        {
+            CurrentPage--;
+            RecordStartForm = articleList.Count - SelectedRecord * (NumberOfPages - (CurrentPage - 1));
+
+            var recordsToShow = articleList.Skip(RecordStartForm).Take(SelectedRecord);
+
+            UpdateCollection(recordsToShow);
+            UpdateEnableState();
+        }
+
+        [RelayCommand]
+        private void LastPage(object obj)
+        {
+            var recordsToSkip = SelectedRecord * (NumberOfPages - 1);
+            UpdateCollection(articleList.Skip(recordsToSkip));
+            CurrentPage = NumberOfPages;
+            UpdateEnableState();
+        }
+
+
+        [RelayCommand]
+        private void FirstPage(object obj)
+        {
+            UpdateCollection(articleList.Take(SelectedRecord));
+            CurrentPage = 1;
+            UpdateEnableState();
+        }
+
+        [RelayCommand]
+        private void NextPage(object obj)
+        {
+            RecordStartForm = CurrentPage * SelectedRecord;
+            var recordsToShow = articleList.Skip(RecordStartForm).Take(SelectedRecord);
+            UpdateCollection(recordsToShow);
+            CurrentPage++;
+            UpdateEnableState();
+        }
+
+        private void UpdateEnableState()
+        {
+            IsFirstEnabled = CurrentPage > 1;
+            isPreviousEnabled = CurrentPage > 1;
+            IsNextEnabled = CurrentPage < NumberOfPages;
+            IsLastEnabled = CurrentPage < NumberOfPages;
+        }
+
+        private void UpdateCollection(IEnumerable<Article> recordsToShow)
+        {
+            ArticlesCollection.Clear();
+            foreach (var item in recordsToShow)
+            {
+                ArticlesCollection.Add(item);
+            }
+        }
 
         [RelayCommand]
         public void LoadData()
         {
             ArticleList = _articleService.GetAll().Result;
-            ArticleCollection = CollectionViewSource.GetDefaultView(ArticleList);
+            ArticleCollection = CollectionViewSource.GetDefaultView(ArticlesCollection);
+            UpdateCollection(articlesCollection.Take(SelectedRecord));
+            UpdateRecordCount();
         }
 
         private bool FilterFunction(object obj)
@@ -49,12 +170,6 @@ namespace ImportApp.WPF.ViewModels
                 return filt != null && (filt.Name.Contains(TextToFilter) || filt.BarCode.Contains(TextToFilter) || filt.Price.ToString() == TextToFilter || filt.ArticleNumber.ToString() == TextToFilter);
             }
             return true;
-        }
-
-        public ArticlesViewModel(IArticleService articleService)
-        {
-            _articleService = articleService;
-            LoadData();
         }
 
     }
