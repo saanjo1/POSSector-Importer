16d02d8ae84b18d6cbabe29d3465d755a5a26a00
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-12-27 13:58:23 +0100

    Add a discount

---
+++ ImportApp.WPF/ViewModels/CreateNewDiscountViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportApp.WPF.ViewModels
+{
+    [ObservableObject]
+    public partial class CreateNewDiscountViewModel
+    {
+
+        private readonly SettingsViewModel _settings;
+
+
+        public CreateNewDiscountViewModel(SettingsViewModel settings)
+        {
+            _settings = settings;
+        }
+
+        [ObservableProperty]
+        private bool isOpen;
+
+
+        [RelayCommand]
+        public void Cancel()
+        {
+            _settings.Cancel();
+        }
+    }
+}
--- ImportApp.WPF/ViewModels/SettingsViewModel.cs
+++ ImportApp.WPF/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace ImportApp.WPF.ViewModels
 {
-    public class SettingsViewModel : BaseViewModel
+    [ObservableObject]
+    public partial class SettingsViewModel : BaseViewModel
     {
+
+        public SettingsViewModel() { }
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(CreateRuleCommand))]
+        private bool isOpen;
+
+        [ObservableProperty]
+        private CreateNewDiscountViewModel ruleViewModel;
+
+
+
+        [RelayCommand]
+        public void CreateRule()
+        {
+            IsOpen = true;
+            this.RuleViewModel = new CreateNewDiscountViewModel(this);
+        }
+
+        [RelayCommand]
+        public void Cancel()
+        {
+            IsOpen = false;
+        }
+
     }
 }
